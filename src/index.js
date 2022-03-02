47613f8292b11ffb6fb4edf9809f5b0b3f68d98a
DanFeliz <93226642+DanFeliz@users.noreply.github.com>
2022-03-02 16:01:48 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,16 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+     let resStr = '';
+    const exprLitArr = expr.match(/.{1,10}/g).map(element => element === '**********' ? element = ' ' : element = element.replace(/^0+/, ''));
+    exprLitArr.forEach(element => {
+      let litMorseStr = element.match(/.{1,2}/g).map(el1 => el1 = el1.replace(/10/i, '.').replace(/11/i, '-')).join(''); 
+      if (litMorseStr === ' ') resStr += ' ';
+      for (let key in MORSE_TABLE) {
+        if (key === litMorseStr) resStr += MORSE_TABLE[key];
+      }
+    })
+  return resStr;
 }
 
 module.exports = {
